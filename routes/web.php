75d71bcfef1751212c7c9bfa6fce2bd76c548b58
Роман Бушуев <bushuev@benefitty.ru>
2024-05-28 22:02:10 +0400

    laravel reverb simple websocket send event

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Events\NewMessage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('send-message', function () {
+    event(new NewMessage('test NewMessage'));
 });
